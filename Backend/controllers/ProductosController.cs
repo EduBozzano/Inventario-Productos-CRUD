bb232f5610ae6374921710b9863927421e94afbc
EduBozzano <bozzanoedu@gmail.com>
2026-02-03 18:54:44 -0300

    add post agregar producto

--- Backend/controllers/ProductosController.cs
+++ Backend/controllers/ProductosController.cs
@@ -24,4 +24,21 @@ public class ProductosController : ControllerBase
         // Esto hace el "SELECT * FROM Productos" automáticamente
         return await _context.Productos.ToListAsync();
     }
+
+    // POST: api/productos
+    [HttpPost]
+    public async Task<ActionResult<Producto>> PostProducto(Producto producto)
+    {
+        // Asignamos la fecha actual en C# para que no sea año 0001
+        producto.FechaCreacion = DateTime.Now;
+
+        // 1. Agregamos el producto a la memoria de EF Core
+        _context.Productos.Add(producto);
+        
+        // 2. Guardamos los cambios en la base de datos SQL (Aquí se genera el ID automático)
+        await _context.SaveChangesAsync();
+
+        // 3. Devolvemos el producto creado con su nuevo ID
+        return CreatedAtAction(nameof(GetProductos), new { id = producto.ID }, producto);
+    }
 }
